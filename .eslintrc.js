9952dbc0f80d15dcdb27103f3bba61774025cd04
Juan Di Toro <ditorojuanf@gmail.com>
2021-02-27 01:42:25 -0400

    fix(all): building not working due to wrong typescript veersion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
 module.exports = {
-  extends: [
+  "extends": [
     "airbnb-typescript/base",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:promise/recommended",
+    "plugin:promise/recommended"
   ],
-  parserOptions: {
-    project: ["./tsconfig.json"],
+  "parserOptions": {
+    "project": [
+      "./tsconfig.json"
+    ]
   },
-  plugins: ["promise"],
-  ignorePatterns: ["dist/**", ".eslintrc.js", "*.js"],
-  rules: {
-    quotes: "off",
-    "@typescript-eslint/quotes": [
-      "error",
-      "double",
-      {
-        avoidEscape: true,
-      },
-    ],
+  "plugins": ["promise"],
+  "ignorePatterns": ["*.js"],
+  "rules": {
+    "quotes": "off",
+    "@typescript-eslint/quotes": ["error", "double", {
+      "avoidEscape": true
+    }],
     "no-restricted-globals": "off",
     "arrow-body-style": ["error", "as-needed"],
     "brace-style": ["error", "1tbs"],
@@ -29,48 +27,39 @@ module.exports = {
     "class-methods-use-this": "off",
     "max-len": ["warn", 200],
     "no-useless-constructor": "off",
-    "object-curly-newline": [
-      "error",
-      {
-        ObjectExpression: { multiline: true },
-        ObjectPattern: { multiline: true },
-        ImportDeclaration: "never",
-        ExportDeclaration: { multiline: true, minProperties: 3 },
-      },
-    ],
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      { overrides: { constructors: "off" } },
-    ],
-    "@typescript-eslint/no-unused-vars": "warn",
+    "object-curly-newline": ["error", {
+      "ObjectExpression": { "multiline": true },
+      "ObjectPattern": { "multiline": true },
+      "ImportDeclaration": "never",
+      "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+    }],
+    "require-await": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-unused-var": "warn",
+    "@typescript-eslint/explicit-member-accessibility": ["error", { "overrides": { "constructors": "off" } }],
     "@typescript-eslint/no-useless-constructor": "off",
     "@typescript-eslint/unbound-method": "warn",
-    "@typescript-eslint/consistent-type-assertions": [
-      "error",
-      { assertionStyle: "as", objectLiteralTypeAssertions: "never" },
-    ],
-    "no-underscore-dangle": ["error", { allow: ["_embedded"] }],
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "@typescript-eslint/consistent-type-assertions": ["error", { "assertionStyle": "as", "objectLiteralTypeAssertions": "never" }],
+    "no-underscore-dangle": ["error", { "allow": ["_embedded"] }],
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
     "prefer-rest-params": "off",
-    "array-callback-return": ["error", { allowImplicit: true }],
+    "array-callback-return": ["error", { "allowImplicit": true }],
     "array-bracket-spacing": ["error", "always"],
     "import/no-unresolved": "off",
     "no-param-reassign": "warn",
     "no-restricted-syntax": [
       "error",
       {
-        selector: "LabeledStatement",
-        message:
-          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+        "selector": "LabeledStatement",
+        "message": "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand."
       },
       {
-        selector: "WithStatement",
-        message:
-          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
-      },
+        "selector": "WithStatement",
+        "message": "`with` is disallowed in strict mode because it makes code impossible to predict and optimize."
+      }
     ],
     "promise/always-return": "off",
     "promise/no-nesting": "error",
-    "promise/no-callback-in-promise": "error",
-  },
-};
+    "promise/no-callback-in-promise": "error"
+  }
+}
