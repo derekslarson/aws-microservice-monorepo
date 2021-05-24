9faeee6d1e0ece45bc505a6b086800adf12d43f4
Derek Larson <derekslarson@gmail.com>
2021-05-24 18:51:45 +0300

    added filler unit tests and set up scripts for runnign them correctly

---
+++ micro-services/auth-service/src/services/tests/mail.service.spec.ts
@@ -0,0 +1,11 @@
+describe("MailService", () => {
+  beforeEach(() => {});
+
+  describe("method goes here", () => {
+    describe("case goes here", () => {
+      it("expectation goes here", () => {
+
+      });
+    });
+  });
+});
