e7ef305f1baf2028c9ea9374c7880d77dc004a56
Derek Larson <derekslarson@gmail.com>
2021-11-02 16:24:11 +0300

    added encoding of data for authorization code request

--- e2e/util.ts
+++ e2e/util.ts
@@ -134,7 +134,7 @@ async function getAuthorizationCode(emailOrId: string, xsrfToken: string, logErr
       throw new Error("Error fetching auth secret");
     }
 
-    const data = `_csrf=${xsrfToken}&username=${emailOrId}&password=${authSecret}`;
+    const data = `_csrf=${encodeURIComponent(xsrfToken)}&username=${encodeURIComponent(emailOrId)}&password=${encodeURIComponent(authSecret)}`;
 
     const queryParameters = {
       response_type: "code",
